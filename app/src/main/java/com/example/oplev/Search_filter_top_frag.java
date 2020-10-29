5a3128fb8f95914df5407909bc08b9636b793ae7
SImon <fridolf.lindhard@gmail.com>
2020-10-29 17:14:16 +0100

    pop tilbage i search

--- app/src/main/java/com/example/oplev/Search_filter_top_frag.java
+++ app/src/main/java/com/example/oplev/Search_filter_top_frag.java
@@ -13,29 +13,31 @@ public class Search_filter_top_frag extends Fragment implements View.OnClickList
     ImageView back;
     static TextView title;
 
-    public Search_filter_top_frag(){
+    public Search_filter_top_frag() {
     }
 
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View root = i.inflate(R.layout.u_settings_frag, container, false);
 
-        title=root.findViewById(R.id.topbar_text);
-        back=root.findViewById(R.id.topbar_arrow);
+        title = root.findViewById(R.id.topbar_text);
+        back = root.findViewById(R.id.topbar_arrow);
 
         back.setOnClickListener(this);
 
         getFragmentManager().beginTransaction().replace(R.id.mainFragmentBox, new Search_filter_bottom_frag(), "uSettingMainBox")
                 .commit();
 
-        return  root;
+        return root;
 
     }
 
 
-
     @Override
     public void onClick(View v) {
+        if (v == back) {
+            getFragmentManager().popBackStack();
 
+        }
     }
 }
