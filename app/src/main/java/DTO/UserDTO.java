73f0992dfef768b3559b24c6dacc802b83b0bfd9
christian <ChristiaNotarmaso@gmail.com>
2020-11-24 21:51:44 +0100

    rettet fejl

--- app/src/main/java/DTO/UserDTO.java
+++ app/src/main/java/DTO/UserDTO.java
@@ -11,7 +11,7 @@ public class UserDTO implements Serializable {
 
     ArrayList<String> pictures = new ArrayList<>(Arrays.asList(null, null, null, null, null, null));
 
-    ArrayList<String> pictures, chatId;
+    ArrayList<String> chatId;
     ArrayList<Integer> events, joinedEvents;
 
     public String getUserId() {
