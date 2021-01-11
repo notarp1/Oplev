f6d3b75cc71b0ffe10cc53937384a496a4eef039
IceMonk3y <fridolf.lindhard@gmai.com>
2021-01-11 13:21:47 +0100

    Basic udkast til list virker nu.

--- app/src/main/java/com/A4/oplev/Like_Hjerte_Side/OwnEvent_frag.java
+++ app/src/main/java/com/A4/oplev/Like_Hjerte_Side/OwnEvent_frag.java
@@ -1,9 +1,208 @@
 package com.A4.oplev.Like_Hjerte_Side;
 
-public class OwnEvent_frag {
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.TaskStackBuilder;
+import androidx.fragment.app.Fragment;
+
+import com.A4.oplev.R;
+import com.A4.oplev._Adapters.LikeSide_Event_Adapter;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import Controller.Listeners.OnSwipeTouchListener;
+import Controller.UserController;
+import DAL.Classes.ChatDAO;
+import DAL.Classes.EventDAO;
+import DTO.UserDTO;
+
+public class OwnEvent_frag extends Fragment {
+    private ListView tilmeldinger_listView;
+    private EventDAO eventDAO;
+    private UserDTO userDTO;
+    private UserController userController;
+    private ArrayList<Date> dates = new ArrayList<>();
+    private ArrayList<Integer> eventApplicantsSize = new ArrayList<>();
+    private ArrayList<String> eventHeaders = new ArrayList<>(), eventOwnerPic = new ArrayList<>(),  eventEventPic = new ArrayList<>(),
+                              eventApplicantPic = new ArrayList<>(), eventFirstApplicants = new ArrayList<>(), eventEventID = new ArrayList<>(),
+                              eventParticipant = new ArrayList<>(), tempEventID = new ArrayList<>(), tempFirstApplicant = new ArrayList<>();
+    private Context mContext;
+
+    private static ArrayList<View> footerViews = new ArrayList<>();
+    private int listviewPosition = 0;
+    boolean eventsReady = false;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater i, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = i.inflate(R.layout.likeside_frag, container, false);
+
+        userController = UserController.getInstance();
+        userDTO = userController.getCurrUser();
+        eventDAO = new EventDAO();
+
+        tilmeldinger_listView = root.findViewById(R.id.beskedListView_tilmeldinger);
+
+        tilmeldinger_listView.setOnTouchListener(new OnSwipeTouchListener(mContext){
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onSwipeLeft() {
+                // Sæt farven på billederne i toppen af skærmen
+                getActivity().findViewById(R.id.besked_back).setVisibility(View.VISIBLE);
+                getActivity().findViewById(R.id.event_back).setVisibility(View.INVISIBLE);
+                getActivity().findViewById(R.id.hjerte_back).setVisibility(View.INVISIBLE);
+
+                // Kreer fragmentet over til hjertesiden
+                getFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter_from_right,
+                        R.anim.exit_to_left).replace(R.id.likeside_frameLayout,new LikesideList_frag())
+                        .commit();
+            }
+        });
 
 
+        // Det her er til ens egne events som andre prøver på at ansøge om at joine
+        // vi prøver på at hente brugeren ind hvis den af en eller anden grund ikke er
+        if (userDTO == null) userDTO = userController.getCurrUser();
+        // vi bruger den her til ikke at instantiere listviewet hvis der bliver hentet noget fra databasen
+        final boolean[] isGettingFromDB = {false};
+        if (userDTO != null) {
+            if (userDTO.getEvents() != null) {
+                // hvis brugeren ikke har nogle events så skal vi sikre os at det ikke går i stå
+                if (userDTO.getEvents().size() == 0) {
+                    eventsReady = true;
+                    setListView_applicants(eventEventPic, eventHeaders, eventOwnerPic, eventFirstApplicants, eventApplicantPic, eventApplicantsSize);
+                    // setChangeListener_tilmeldinger();
 
+                }
 
+                // vi itererer over alle events
+                for (int j = 0; j < userDTO.getEvents().size(); j++) {
+                    eventDAO.getEvent(event -> {
+                        if (event != null) {
+                            // ternary operator for at få den første applicant
+                            String firstApplicant = event.getApplicants().size() == 0 ? "" : event.getApplicants().get(0);
+                            // Hvis der ikke er nogle applicants så indsætter vi disse værdier
+                            if (firstApplicant.equals("")) {
+                                eventApplicantPic.add("");
+                                eventApplicantsSize.add(event.getApplicants().size());
+                                eventHeaders.add(event.getTitle());
+                                eventOwnerPic.add(event.getOwnerPic());
+                                eventEventPic.add(event.getEventPic());
+                                eventFirstApplicants.add(firstApplicant);
+                                eventEventID.add(event.getEventId());
+                                eventParticipant.add(event.getParticipant());
+                                // hvis det er den sidste event OG vi ikke henter noget fra databasen så kan vi køre den her kode
+                                if (eventApplicantPic.size() == userDTO.getEvents().size() && !isGettingFromDB[0]) {
+                                    // events er loadet færdigt
+                                    eventsReady = true;
+                                    // hvis chats er loadet færdigt så opsæt listviewet og listeners
+                                    setListView_applicants(eventEventPic, eventHeaders, eventOwnerPic, eventFirstApplicants, eventApplicantPic, eventApplicantsSize);
+                                    // setChangeListener_tilmeldinger();
+                                }
+                            } else {
+                                // hvis der er mindst 1 applicant så skal vi hente noget ind fra databasen
+                                isGettingFromDB[0] = true;
+                                // vi henter applicanten og indsætter personens værdier
+                                userController.getUser(user -> {
+                                    eventApplicantPic.add(user.getUserPicture());
+                                    eventApplicantsSize.add(event.getApplicants().size());
+                                    eventHeaders.add(event.getTitle());
+                                    eventOwnerPic.add(event.getOwnerPic());
+                                    eventEventPic.add(event.getEventPic());
+                                    eventFirstApplicants.add(firstApplicant);
+                                    eventEventID.add(event.getEventId());
+                                    eventParticipant.add(event.getParticipant());
+                                    // nu har vi hentet færdigt
+                                    isGettingFromDB[0] = false;
+                                    // hvis det var sidste event så kør dette
+                                    if (eventApplicantPic.size() == userDTO.getEvents().size()) {
+                                        eventsReady = true;
+                                        // hvis chats er færdige så opsæt listview og listeners
+                                        setListView_applicants(eventEventPic, eventHeaders, eventOwnerPic, eventFirstApplicants, eventApplicantPic, eventApplicantsSize);
+                                            //setChangeListener_tilmeldinger();
+                                    }
+                                }, event.getApplicants().get(0));
+                            }
+                        }
+                    }, userDTO.getEvents().get(j));
+                }
+            }
+        }
+        return root;
+    }
 
+        public void setListView_applicants(@NonNull ArrayList < String > eventEventPic, @NonNull ArrayList < String > eventHeaders, @NonNull ArrayList < String > eventOwnerPic, @NonNull ArrayList < String > eventFirstApplicants, @NonNull ArrayList < String > eventApplicantPic, @NonNull ArrayList < Integer > eventApplicantsSize)
+        {
+            if (mContext != null) {
+                ArrayList<String> tempEventPic = new ArrayList<>(), tempEventHeaders = new ArrayList<>(), tempEventOwnerPic = new ArrayList<>(), tempEventFirstApplicants = new ArrayList<>(), tempEventApplicantPic = new ArrayList<>(), tempEventID = new ArrayList<>();
+                ArrayList<Integer> tempEventApplicantsSize = new ArrayList<>();
+
+                for (int i = 0; i < eventHeaders.size(); i++) {
+                        tempEventPic.add(eventEventPic.get(i));
+                        tempEventHeaders.add(eventHeaders.get(i));
+                        tempEventApplicantPic.add(eventApplicantPic.get(i));
+                        tempEventApplicantsSize.add(eventApplicantsSize.get(i));
+                        tempEventOwnerPic.add(eventOwnerPic.get(i));
+                        tempEventFirstApplicants.add(eventFirstApplicants.get(i));
+                        tempEventID.add(eventEventID.get(i));
+                }
+                this.tempEventID = tempEventID;
+                this.tempFirstApplicant = tempEventFirstApplicants;
+
+                LikeSide_Event_Adapter eventAdapter = new LikeSide_Event_Adapter(mContext, tempEventPic, tempEventHeaders, tempEventOwnerPic, tempEventFirstApplicants, tempEventApplicantPic, tempEventApplicantsSize);
+                tilmeldinger_listView.setAdapter(eventAdapter);
+            }
+        }
+
+    @Override
+    public void onAttach(@NotNull Context context){
+        super.onAttach(context);
+        this.mContext = context;
+    }
+
+    @Override
+    public void onDetach(){
+        this.mContext = null;
+        super.onDetach();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+    // Skal ændres til noget baggrunds halløjsa senere hen
+    private void sendNoti(){
+        Intent i = new Intent(mContext, Activity_Likeside.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(mContext);
+        stackBuilder.addNextIntentWithParentStack(i);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext, "0")
+                .setSmallIcon(R.drawable.chat)
+                .setContentTitle("KOM TILBAGE")
+                .setContentText("I miss you bro")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(mContext);
+        builder.setContentIntent(resultPendingIntent);
+        notificationManager.notify(0, builder.build());
+    }
 }
+
