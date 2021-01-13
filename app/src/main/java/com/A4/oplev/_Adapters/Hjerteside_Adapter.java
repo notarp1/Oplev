2b0b7530ef89d75dab9fcc6ca05c0b630905308b
ColleDK <s195490@student.dtu.dk>
2021-01-13 13:59:52 +0100

    Hjerteside adapter lavet + der bliver indlæst events nu (skal skifte fra ens egne events til likede events senere)

--- app/src/main/java/com/A4/oplev/_Adapters/Hjerteside_Adapter.java
+++ app/src/main/java/com/A4/oplev/_Adapters/Hjerteside_Adapter.java
@@ -5,25 +5,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.A4.oplev.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Hjerteside_Adapter extends ArrayAdapter<String> {
     private Context mContext;
-    private List<String> nameList = new ArrayList<>(), ageList = new ArrayList<>(), profilePictures = new ArrayList<>(), headerList = new ArrayList<>(), placementList = new ArrayList<>(), timeList = new ArrayList<>(), eventPictureList = new ArrayList<>();
+    private List<String> nameList = new ArrayList<>(), ageList = new ArrayList<>(), profilePictures = new ArrayList<>(), headerList = new ArrayList<>(), placementList = new ArrayList<>(), eventPictureList = new ArrayList<>();
     private List<Integer> priceList = new ArrayList<>();
+    private List<Date> timeList = new ArrayList<>();
 
     public Hjerteside_Adapter(@NonNull Context context, @NonNull ArrayList<String> headerList,
-                              @NonNull ArrayList<String> names, @NonNull ArrayList<String> ageList,
+                              @NonNull ArrayList<String> names,
                               @NonNull ArrayList<String> profilePictures, @NonNull ArrayList<String> placementList,
-                              @NonNull ArrayList<String> timeList, @NonNull ArrayList<Integer> priceList, @NonNull ArrayList<String> eventPictureList) {
+                              @NonNull ArrayList<Date> timeList, @NonNull ArrayList<Integer> priceList, @NonNull ArrayList<String> eventPictureList, @NonNull ArrayList<String> ageList) {
         super(context, 0 , headerList);
         this.mContext = context;
         this.headerList = headerList;
@@ -48,11 +52,9 @@ public class Hjerteside_Adapter extends ArrayAdapter<String> {
         String currentHeader = headerList.get(position);
         String currentName = nameList.get(position);
         String currentAge = ageList.get(position);
-        String profilePic = profilePictures.get(position);
-        String currentEventPic = eventPictureList.get(position);
-        String currentPlacement = placementList.get(position);
-        String currentTime = timeList.get(position);
-        int currentPrice = priceList.get(position);
+        //String currentPlacement = placementList.get(position);
+        //Date currentTime = timeList.get(position);
+        //int currentPrice = priceList.get(position);
 
 
         TextView header = (TextView) listItem.findViewById(R.id.hjertside_overskrift);
@@ -61,6 +63,21 @@ public class Hjerteside_Adapter extends ArrayAdapter<String> {
         TextView name = (TextView) listItem.findViewById(R.id.hjerteside_medhvem);
         name.setText("Med " + currentName + ", " + currentAge);
 
+        ImageView profilBillede = (ImageView) listItem.findViewById(R.id.hjerteside_profilbillede);
+        Picasso.get().load(profilePictures.get(position))
+                .resize(mContext.getDisplay().getWidth(), mContext.getDisplay().getHeight() / 2 + 200)
+                .centerCrop()
+                .placeholder(R.drawable.load2)
+                .error(R.drawable.question)
+                .into(profilBillede);
+
+        ImageView eventBillede = (ImageView) listItem.findViewById(R.id.hjerteside_billede);
+        Picasso.get().load(eventPictureList.get(position))
+                //.resize(mContext.getDisplay().getWidth(), mContext.getDisplay().getHeight() / 2 + 200)
+                //.centerCrop()
+                .placeholder(R.drawable.load2)
+                .error(R.drawable.question)
+                .into(eventBillede);
 
         return listItem;
     }
